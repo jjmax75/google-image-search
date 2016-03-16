46789eacc35330a0a625531a52a504a005a75291
John Behan <behanj@gmail.com>
2016-03-16 15:15:13 +0700

    module fully functional, readme updated

--- index.js
+++ index.js
@@ -5,30 +5,51 @@ require('dotenv').config();
 
 /**
  * Retrieves a list of image search results from Google
+ * @param (String) searchTerm
+ * @param (Function) callback (function to call once results are processed)
+ * @param (Number) -optional- start (starting from what result)
+ * @param (Number) -optional- num (how many results to return, 1 - 10)
  *
+ * @return (Object)
  *
  */
-var search = 'panda';
 
-var options = {
-  host: 'www.googleapis.com',
-  path: '/customsearch/v1?key=' + process.env.CSE_API_KEY + '&cx=' + process.env.CSE_ID + '&q=' + search
-};
+function getImageSearchResults(searchTerm, callback, start, num) {
+  start = start < 0 || start > 90 || typeof(start) === 'undefined' ? 0 : start;
+  num = num < 1 || num > 10 || typeof(num) === 'undefined' ? 10 : num;
 
-console.log('Options: ' + options.path);
+  if (!searchTerm) {
+    console.error('No search term');
+  }
 
-var callback = function(response) {
-  var str = '';
+  var parameters = '&q=' + searchTerm;
+  parameters += '&searchType=image';
+  parameters += start ? '&start=' + start : '';
+  parameters += '&num=' + num;
 
-  //another chunk of data has been recieved, so append it to `str`
-  response.on('data', function (chunk) {
-    str += chunk;
+  var options = {
+    host: 'www.googleapis.com',
+    path: '/customsearch/v1?key=' + process.env.CSE_API_KEY + '&cx=' + process.env.CSE_ID + parameters
+  };
+
+  var result = '';
+
+  https.get(options, function(response) {
+    response.setEncoding('utf8');
+
+    response.on('data', function(data) {
+      result += data;
+    });
+
+    response.on('end', function() {
+      var data = JSON.parse(result);
+      var resultsArray = [];
+
+      data.items.forEach(function(item) {
+        resultsArray.push(item);
+      });
+
+      callback(resultsArray);
+    });
   });
-
-  //the whole response has been recieved, so we just print it out here
-  response.on('end', function () {
-    console.log(str);
-  });
-};
-
-https.request(options, callback).end();
+}
